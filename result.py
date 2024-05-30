1f973c24289de02513c490953a2a2e7c65c639af
StanSmits17 <stan_smits.17@hotmail.com>
2024-05-30 17:34:57 +0200

    Update result.py

--- result.py
+++ result.py
@@ -76,7 +76,7 @@ def show_result():
     cluster_data = data.iloc[kmeans.labels_ == cluster_label[0]]['Druivensoort']
     cluster_df = pd.DataFrame(cluster_data, columns=['Druivensoort']).reset_index(drop=True)
     st.subheader("Grapes that are very similar to your best fit")
-    st.table(cluster_df['Druivensoort'])
+    st.table(cluster_df['Druivensoort'].reset_index(drop=True), index=False)
 
     labels = data.columns[1:]
     stats_user = user_data.iloc[0, 1:].tolist()
