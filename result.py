edfba08980809903c23b92dc317e8bd459b12bdc
StanSmits17 <stan_smits.17@hotmail.com>
2024-05-30 16:57:36 +0200

    Update result.py

--- result.py
+++ result.py
@@ -68,15 +68,15 @@ def show_result():
         match_percentage = round(match_score * 100, 2)
         matches.append((data.iloc[index]['Druivensoort'], match_percentage))
 
-    match_df = pd.DataFrame(matches, columns=['Druivensoort', 'Match Percentage'])
-    st.table(match_df.style.hide(axis='index'))
+    match_df = pd.DataFrame(matches, columns=['Druivensoort', 'Match Percentage']).reset_index(drop=True)
+    st.table(match_df)
 
     kmeans = KMeans(n_clusters=4, random_state=0).fit(data.drop('Druivensoort', axis=1))
     cluster_label = kmeans.predict(user_data.drop('Druivensoort', axis=1))
     cluster_data = data.iloc[kmeans.labels_ == cluster_label[0]]['Druivensoort']
-    cluster_df = pd.DataFrame(cluster_data, columns=['Druivensoort'])
+    cluster_df = pd.DataFrame(cluster_data, columns=['Druivensoort']).reset_index(drop=True)
     st.subheader("Grapes that are very similar to your best fit")
-    st.table(cluster_df.style.hide(axis='index'))
+    st.table(cluster_df)
 
     labels = data.columns[1:]
     stats_user = user_data.iloc[0, 1:].tolist()
